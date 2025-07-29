2b4640165c3e8473508ca9068c45b384203f31ff
Mazen Atlam <mazenatef5510@gmail.com>
2025-07-30 01:16:28 +0300

    Create Admin repo and service classes

---
+++ CCMS/CCMS.BLL/Services/Implementation/AdministratorService.cs
@@ -0,0 +1,41 @@
+﻿using CCMS.DAL.Enums;
+//using CCMS.BLL.ModelVM.Employee;
+using CCMS.BLL.Services.Abstraction;
+
+namespace CCMS.BLL.Services.Implementation
+{
+    public class AdministratorService : IAdministratorService
+    {
+        private readonly IAdministratorService adminService = new AdministratorService();
+
+        public string? Create(/*CreateEmployee emp, */string creatingUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public /*(List<EmployeeDTO>, */string?/*)*/ GetAllEmployeesByRole(/*EmployeeRole role*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public /*(EmployeeDTO, */string?/*)*/ GetAdminByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public /*(List<EmployeeDTO>, */string?/*)*/ GetAllEmployeesCrearedBy(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? Update(/*EmployeeDTO emp, */string modifyingUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? Delete(int id, string modifyingUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CCMS/CCMS.DAL/Repository/Abstraction/IAdministratorRepo.cs
@@ -0,0 +1,26 @@
+﻿using CCMS.DAL.Entities;
+
+namespace CCMS.DAL.Repository.Abstraction
+{
+    public interface IAdministratorRepo
+    {
+        public void Add(Employee admin);
+
+        public List<Employee> GetAllAdmins();
+
+        //// From Doctor service
+        //public List<Employee> GetAllDoctors();
+
+        //// From LabDoctor service
+        //public List<Employee> GetAllLabDoctors();
+
+        //// From BiomedicalEngineer service
+        //public List<Employee> GetAllBiomedicalEngineers();
+
+        public Employee GetAdminByID(int id);
+
+        public List<Employee> GetAllEmployeesCrearedBy(int id);
+
+        public void Save();
+    }
+}

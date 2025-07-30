1affde1781f05cdd3f47e2bcc9c17ede5dd4d236
Mazen Atlam <mazenatef5510@gmail.com>
2025-07-30 04:23:34 +0300

    Add biomedical engineer service

---
+++ CCMS/CCMS.BLL/Services/Abstraction/IBiomedicalEngineerService.cs
@@ -0,0 +1,19 @@
+﻿using CCMS.BLL.ModelVM.MedicalDevice;
+
+namespace CCMS.BLL.Services.Abstraction
+{
+    public interface IBiomedicalEngineerService
+    {
+        public string? Create(/*CreateEmployee emp, */string creatingUser);
+
+        public /*(List<EmployeeDTO>?, */string?/*)*/ GetAllBiomedicalEngineers();
+
+        public (List<MedicalDeviceDTO>?, string?) GetAllMedicalDevicesWorksOn(int id);
+
+        public /*(EmployeeDTO?, */string?/*)*/ GetBiomedicalEngineerByID(int id);
+
+        public string? Update(/*EmployeeDTO emp, */string modifyingUser);
+
+        public string? Delete(int id, string modifyingUser);
+    }
+}

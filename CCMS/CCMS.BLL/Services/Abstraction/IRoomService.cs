3b9c44b7ed040a8567db65e68a158a85c02aa879
Mazen Atlam <mazenatef5510@gmail.com>
2025-07-30 06:23:43 +0300

    Add Room service

---
+++ CCMS/CCMS.BLL/Services/Abstraction/IRoomService.cs
@@ -0,0 +1,24 @@
+﻿using CCMS.BLL.ModelVM.MedicalDevice;
+using CCMS.DAL.Enums;
+
+namespace CCMS.BLL.Services.Abstraction
+{
+    public interface IRoomService
+    {
+        public string? Create(/*CreateRoom room, */string creatingUser);
+
+        public /*(List<RoomDTO>?, */string?/*)*/ GetAllRooms();
+
+        public (List<MedicalDeviceDTO>?, string?) GetAllMedicalDevicesIn(string roomNum);
+
+        public /*(List<RoomDTO>?, */string?/*)*/ GetAllRoomsByBuildingAndFloorNumber(string buildingNum, int floorNum);
+
+        public /*(RoomDTO?, */string?/*)*/ GetRoomByNumber(string roomNum);
+
+        public string? UpdateStatus (string roomNum, Rstatus newStatus, string modifyingUser);
+
+        public string? Update(/*RoomDTO room, */string modifyingUser);
+
+        public string? Delete(string roomNum, string modifyingUser);
+    }
+}
---
+++ CCMS/CCMS.BLL/Services/Implementation/RoomService.cs
@@ -0,0 +1,49 @@
+﻿using CCMS.BLL.ModelVM.MedicalDevice;
+using CCMS.BLL.Services.Abstraction;
+using CCMS.DAL.Enums;
+
+namespace CCMS.BLL.Services.Implementation
+{
+    public class RoomService : IRoomService
+    {
+        public string? Create(/*CreateRoom room, */string creatingUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public /*(List<RoomDTO>?, */string?/*)*/ GetAllRooms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public (List<MedicalDeviceDTO>?, string?) GetAllMedicalDevicesIn(string roomNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public /*(List<RoomDTO>?, */string?/*)*/ GetAllRoomsByBuildingAndFloorNumber(string buildingNum, int floorNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public /*(RoomDTO?, */string?/*)*/ GetRoomByNumber(string roomNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? UpdateStatus(string roomNum, Rstatus newStatus, string modifyingUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? Update(/*RoomDTO room, */string modifyingUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? Delete(string roomNum, string modifyingUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

e3fb1c2219fe6e336139c85d54de67f170a3494a
Mazen Atlam <mazenatef5510@gmail.com>
2025-08-03 21:14:27 +0300

    Implement medical device repo

--- CCMS/CCMS.DAL/Repository/Implementation/MedicalDeviceRepo.cs
+++ CCMS/CCMS.DAL/Repository/Implementation/MedicalDeviceRepo.cs
@@ -6,31 +6,31 @@ namespace CCMS.DAL.Repository.Implementation
 {
     public class MedicalDeviceRepo : IMedicalDeviceRepo
     {
-        //private readonly CcmsDbContext medicalDeviceCcmsDbContext = new();
+        private readonly CcmsDbContext medicalDeviceDbContext;
+        private readonly CcmsDbContext biomedicalEngineer_medicalDeviceDbContext;
 
-        public void Add(MedicalDevice md)
-        {
-            throw new NotImplementedException();
-        }
+        public MedicalDeviceRepo(CcmsDbContext db) => medicalDeviceDbContext = biomedicalEngineer_medicalDeviceDbContext = db;
+
+        public void Add(MedicalDevice md) => medicalDeviceDbContext.Add(md);
 
         public List<MedicalDevice> GetAllMedicalDevices()
-        {
-            throw new NotImplementedException();
-        }
+            => medicalDeviceDbContext.MedicalDevices.Where(device => !device.IsDeleted).ToList();
 
         public MedicalDevice GetMedicalDeviceBySerialNumber(string serialNum)
         {
-            throw new NotImplementedException();
+            MedicalDevice? medicalDevice = medicalDeviceDbContext.MedicalDevices.Where(device => device.SerialNumber == serialNum && !device.IsDeleted).FirstOrDefault();
+
+            return medicalDevice == null
+                ? throw new ArgumentException($"There is no medical device with the serial number = {serialNum} .", "serialNum")
+                : medicalDevice;
         }
 
         public List<BiomedicalEngineer> GetAllBiomedicalEngineersWorksOn(string serialNum)
-        {
-            throw new NotImplementedException();
-        }
+            => biomedicalEngineer_medicalDeviceDbContext.BiomedicalEngineers_MedicalDevices
+            .Where( join => join.SerialNumber == serialNum && !join.MedicalDevice.IsDeleted)
+            .Select(join => join.BiomedicalEngineer)
+            .ToList();
 
-        public void Save()
-        {
-            throw new NotImplementedException();
-        }
+        public void Save() => medicalDeviceDbContext.SaveChanges();
     }
 }

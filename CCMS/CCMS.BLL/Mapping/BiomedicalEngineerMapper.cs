2e96d5b104795a78a368074aaeda70d714b72547
Mazen Atlam <mazenatef5510@gmail.com>
2025-08-05 00:37:31 +0300

    Add BiomedicalEnginner and MedicalDevice Mappers

---
+++ CCMS/CCMS.BLL/Mapping/BiomedicalEngineerMapper.cs
@@ -0,0 +1,17 @@
+﻿using CCMS.BLL.ModelVM.Employee;
+using CCMS.DAL.Entities;
+using Riok.Mapperly.Abstractions;
+
+namespace CCMS.BLL.Mapping
+{
+    [Mapper]
+    public static partial class BiomedicalEngineerMapper
+    {
+        // Map BiomedicalEngineer -> EmployeeDTO
+        public static partial EmployeeDTO ToResponseDto(BiomedicalEngineer biomedicalEngineer);
+        public static partial List<EmployeeDTO> ToListResponseDto(List<BiomedicalEngineer> biomedicalEngineers);
+
+        // Map EmployeeDTO -> BiomedicalEngineer
+        public static partial BiomedicalEngineer ToEntity(EmployeeDTO mdDeviceDTO);
+    }
+}

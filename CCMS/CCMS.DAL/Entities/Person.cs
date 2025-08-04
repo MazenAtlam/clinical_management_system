799ea6b1be0fc442e4dfcc8a0c7066bf76ab74cd
scarletMoon <50997220+scarletM00n@users.noreply.github.com>
2025-08-04 15:50:26 +0300

    Update Person.cs

--- CCMS/CCMS.DAL/Entities/Person.cs
+++ CCMS/CCMS.DAL/Entities/Person.cs
@@ -7,31 +7,31 @@ namespace CCMS.DAL.Entities
     public abstract class Person : Base
     {
         [Key]
-        public int Id { get; set; }
+        public int Id { get;protected set; }
 
         [Required]
         [MaxLength(50)]
-        public string FName { get; set; }
+        public string FName { get;protected set; }
 
         [MaxLength(50)]
-        public string MidName { get; set; }
+        public string MidName { get;protected set; }
 
         [Required]
         [MaxLength(50)]
-        public string LName { get; set; }
+        public string LName { get;protected set; }
 
         [Required]
         [StringLength(14, MinimumLength = 14)] 
-        public string SSN { get; set; }
+        public string SSN { get;protected set; }
 
         [Required]
-        public Gender Gender { get; set; }
+        public Gender Gender { get;protected set; }
 
         [Required]
         [DataType(DataType.Date)]
-        public DateTime BirthDate { get; set; }
+        public DateTime BirthDate { get;protected set; }
 
         [Required]
-        public PersonType PType { get; set; }
+        public PersonType PType { get;protected set; }
     }
 }

2164c04b7156aee60c30e355d72d105aee0fef5f
Wafaeyy <youssefmohamedwafaey@gmail.com>
2025-08-09 12:22:03 +0300

    LandingPage

--- CCMS/CCMS.DAL/Database/CcmsDbContext.cs
+++ CCMS/CCMS.DAL/Database/CcmsDbContext.cs
@@ -25,15 +25,6 @@ namespace CCMS.DAL.Database
         public DbSet<PhoneNumber> PhoneNumbers { get; set; }
         public DbSet<Room> rooms { get; set; }
         public DbSet<Scan> scans { get; set; }
-        public DbSet<FamilyMember> familyMembers { get; set; }
-        public DbSet<MedicalHistory> medicalHistories { get; set; }
-        public DbSet<Book> books { get; set; }
-        public DbSet<PateintFamilyJoin> pateintFamilyJoins { get; set; }
-        public DbSet<Doctor> doctors { get; set; }
-        public DbSet<MedicalDevice> MedicalDevices { get; set; }
-        public DbSet<LabDoctor> labDoctors { get; set; }
-        public DbSet<BiomedicalEngineer> biomedicalEngineers { get; set; }
         public DbSet<WorkingSlot> workingSlots { get; set; }
-        public DbSet<BiomedicalEngineer_MedicalDevice> BiomedicalEngineers_MedicalDevices { get; set; }
     }
 }
---
+++ CCMS/CCMS.PLL/Controllers/PatientController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CCMS.PLL.Controllers
+{
+    public class PatientController: Controller
+    {
+    }
+}

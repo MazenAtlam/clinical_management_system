87b92500b9bbff4d93243f5dddc8f8fedefa7e94
Wafaeyy <youssefmohamedwafaey@gmail.com>
2025-08-16 00:59:42 +0300

    fix common errors, finalise design

--- CCMS/CCMS.BLL/ModelVM/Patient/PatientDTO.cs
+++ CCMS/CCMS.BLL/ModelVM/Patient/PatientDTO.cs
@@ -1,9 +1,14 @@
+using CCMS.DAL.Entities;
 using CCMS.DAL.Enums;
 
 namespace CCMS.BLL.ModelVM.Patient
 {
     public class PatientDTO
     {
+        public PatientDTO()
+        {
+        }
+
         public int UID { get; set; }
         public string FName { get; set; }
         public string? MidName { get; set; }
@@ -12,5 +17,25 @@ namespace CCMS.BLL.ModelVM.Patient
         public Gender Gender { get; set; }
         public DateOnly BirthDate { get; set; }
         public BloodType BloodType { get; set; }
+        public List<PhoneNumber> PhoneNumbers { get; set; }
+        public List<Address> Addresses { get; set; }
+
+        public PatientDTO(int uID, string fName, string? midName, string lName, string ssn, Gender gender, DateOnly birthDate, BloodType bloodType, List<PhoneNumber> phoneNumbers, List<Address> addresses)
+        {
+            UID = uID;
+            FName = fName;
+            MidName = midName;
+            LName = lName;
+            Ssn = ssn;
+            Gender = gender;
+            BirthDate = birthDate;
+            BloodType = bloodType;
+            PhoneNumbers = phoneNumbers;
+            Addresses = addresses;
+        }
+        public string GetFullName()
+        {
+            return FName + " " + MidName + " " + LName;
+        }
     }
 }
--- CCMS/CCMS.PLL/Controllers/PatientController.cs
+++ CCMS/CCMS.PLL/Controllers/PatientController.cs
@@ -1,18 +1,20 @@
 ﻿
-using CCMS.BLL.Services.Implementation;
+using CCMS.BLL.ModelVM.Patient;
+using CCMS.BLL.Services.Abstraction;
+using CCMS.DAL.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CCMS.PLL.Controllers
 {
     public class PatientController: Controller
     {
-        // make interface not service
-        //private readonly PatientService _patientService;
+        //ADD MAPPERS
+        private readonly IPatientService _patientService;
 
-        //public PatientController(PatientService patientService)
-        //{
-        //    _patientService = patientService;
-        //}
+        public PatientController(IPatientService patientService)
+        {
+            _patientService = patientService;
+        }
 
         public ActionResult PatientLogin()
         { 
@@ -23,17 +25,19 @@ namespace CCMS.PLL.Controllers
             return View();
         }
         //show photo and details and give permission to edit
-        public ActionResult PatientProfile()
+        public ActionResult PatientProfile(int id)
         {
-            return View();
+            Patient patient = _patientService.GetById(id);
+            PatientDTO patientDTO = new PatientDTO(patient.UID, patient.FName, patient.MidName, patient.LName, patient.Ssn, patient.Gender, patient.BirthDate, patient.BloodType,patient.PhoneNumbers,patient.Addresses);
+            ViewBag.Appointment = _patientService.GetPatientAppointments(id);
+            ViewBag.Scans = _patientService.GetScansOfPatient(id);
+            ViewBag.FamilyMembers = _patientService.GetFamilyMembersOfPatient(id);
+            return View(patientDTO);
         }
-        public ActionResult EditPatientProfile()
+        public ActionResult EditPatientProfile(PatientDTO patientDTO)
         {
-            return View();
-        }
-        public ActionResult PatientDashBoard()
-        {
-            return View();
+            _patientService.Update(patientDTO);
+            return RedirectToAction("PatientProfile",patientDTO.UID);
         }
         //show doctors based on majors and ratings
         public ActionResult PGet_Doctors()

3479765f283c20e7ca077109b40b9328d47476d9
Wafaeyy <youssefmohamedwafaey@gmail.com>
2025-08-20 19:56:29 +0300

    background job setup

--- CCMS/CCMS.PLL/Program.cs
+++ CCMS/CCMS.PLL/Program.cs
@@ -5,6 +5,7 @@ using CCMS.DAL.Entities;
 using CCMS.DAL.Repository.Abstraction;
 using CCMS.DAL.Repository.Implementation;
 using DotNetEnv;
+using Hangfire;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -88,6 +89,10 @@ namespace CCMS.PLL
             builder.Services.AddScoped<IPatientService, PatientService>();
             builder.Services.AddScoped<IApplicationUserService, ApplicationUserService>();
 
+            //background job
+            builder.Services.AddHangfire(x => x.UseSqlServerStorage(connectionString));
+            builder.Services.AddHangfireServer();
+
 
             var app = builder.Build();
 
@@ -110,6 +115,7 @@ namespace CCMS.PLL
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+            app.UseHangfireDashboard("/CCMS");
             app.Run();
         }
     }
